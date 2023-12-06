51a51658073bae50e5a2113eeeef7f2910d40f8b
Mohamed Lahbib <89697853+Med-Lahbib@users.noreply.github.com>
2023-12-06 22:45:01 +0100

    seotttt

--- js/data.js
+++ js/data.js
@@ -2,111 +2,111 @@ export const data = [
     {
         "bgColor": "radial-gradient(circle, rgba(255,95,95,1) 0%, rgba(25,2,92,1) 100%)",
         "name": "Blazing Lion",
-        "imageURL": "./img/items/0.png"
+        "imageURL": "./img/items/110.webp"
     },
     {
         "bgColor": "radial-gradient(circle, rgba(255,95,95,1) 0%, rgba(25,2,92,1) 100%)",
         "name": "Chocolate Chip",
-        "imageURL": "./img/items/19.png"
+        "imageURL": "./img/items/119.webp"
     },
     {
         "bgColor": "radial-gradient(circle, rgba(255,95,95,1) 0%, rgba(25,2,92,1) 100%)",
         "name": "Shadow Dragon",
-        "imageURL": "./img/items/1.png"
+        "imageURL": "./img/items/111.webp"
     },
     {
         "bgColor": "radial-gradient(circle, rgba(255,95,95,1) 0%, rgba(25,2,92,1) 100%)",
         "name": "Frost Dragon",
-        "imageURL": "./img/items/2.png"
+        "imageURL": "./img/items/112.webp"
     },
     {
         "bgColor": "radial-gradient(circle, rgba(255,95,95,1) 0%, rgba(25,2,92,1) 100%)",
         "name": "Bat Dragon",
-        "imageURL": "./img/items/3.png"
+        "imageURL": "./img/items/113.webp"
     },
     {
         "bgColor": "radial-gradient(circle, rgba(255,95,95,1) 0%, rgba(25,2,92,1) 100%)",
         "name": "Lavender Dragon",
-        "imageURL": "./img/items/4.png"
+        "imageURL": "./img/items/114.webp"
     },
     {
         "bgColor": "radial-gradient(circle, rgba(255,95,95,1) 0%, rgba(25,2,92,1) 100%)",
         "name": "Vampire Dragon",
-        "imageURL": "./img/items/vd.png"
+        "imageURL": "./img/items/vd.webp"
     },
     {
         "bgColor": "radial-gradient(circle, rgba(255,95,95,1) 0%, rgba(25,2,92,1) 100%)",
         "name": "Unicorn",
-        "imageURL": "./img/items/5.png"
+        "imageURL": "./img/items/115.webp"
     },
     {
         "bgColor": "radial-gradient(circle, rgba(255,95,95,1) 0%, rgba(25,2,92,1) 100%)",
         "name": "Alicorn",
-        "imageURL": "./img/items/6.png"
+        "imageURL": "./img/items/116.webp"
     },
     {
         "bgColor": "radial-gradient(circle, rgba(255,95,95,1) 0%, rgba(25,2,92,1) 100%)",
         "name": "Diamond Unicorn",
-        "imageURL": "./img/items/7.png"
+        "imageURL": "./img/items/117.webp"
     },
     {
         "bgColor": "radial-gradient(circle, rgba(255,95,95,1) 0%, rgba(25,2,92,1) 100%)",
         "name": "Evil Unicorn",
-        "imageURL": "./img/items/8.png"
+        "imageURL": "./img/items/118.webp"
     },
     {
         "bgColor": "radial-gradient(circle, rgba(255,95,95,1) 0%, rgba(25,2,92,1) 100%)",
         "name": "Frost Fury",
-        "imageURL": "./img/items/9.png"
+        "imageURL": "./img/items/119.webp"
     },
     {
         "bgColor": "radial-gradient(circle, rgba(255,95,95,1) 0%, rgba(25,2,92,1) 100%)",
         "name": "Golden Penguin",
-        "imageURL": "./img/items/10.png"
+        "imageURL": "./img/items/120.webp"
     },
     {
         "bgColor": "radial-gradient(circle, rgba(255,95,95,1) 0%, rgba(25,2,92,1) 100%)",
         "name": "Kitsune",
-        "imageURL": "./img/items/11.png"
+        "imageURL": "./img/items/121.webp"
     },
     {
         "bgColor": "radial-gradient(circle, rgba(255,95,95,1) 0%, rgba(25,2,92,1) 100%)",
         "name": "Monkey King",
-        "imageURL": "./img/items/12.png"
+        "imageURL": "./img/items/122.webp"
     },
     {
         "bgColor": "radial-gradient(circle, rgba(255,95,95,1) 0%, rgba(25,2,92,1) 100%)",
         "name": "Giraffe",
-        "imageURL": "./img/items/13.png"
+        "imageURL": "./img/items/123.webp"
     },
     {
         "bgColor": "radial-gradient(circle, rgba(255,95,95,1) 0%, rgba(25,2,92,1) 100%)",
         "name": "Bee",
-        "imageURL": "./img/items/14.png"
+        "imageURL": "./img/items/124.webp"
     },
     {
         "bgColor": "radial-gradient(circle, rgba(255,95,95,1) 0%, rgba(25,2,92,1) 100%)",
         "name": "Owl",
-        "imageURL": "./img/items/15.png"
+        "imageURL": "./img/items/125.webp"
     },
     {
         "bgColor": "radial-gradient(circle, rgba(255,95,95,1) 0%, rgba(25,2,92,1) 100%)",
         "name": "Parrot",
-        "imageURL": "./img/items/16.png"
+        "imageURL": "./img/items/126.webp"
     },
     {
         "bgColor": "radial-gradient(circle, rgba(255,95,95,1) 0%, rgba(25,2,92,1) 100%)",
         "name": "Axolotl",
-        "imageURL": "./img/items/17.png"
+        "imageURL": "./img/items/127.webp"
     },
     {
         "bgColor": "radial-gradient(circle, rgba(255,95,95,1) 0%, rgba(25,2,92,1) 100%)",
         "name": "Kangaroo",
-        "imageURL": "./img/items/18.png"
+        "imageURL": "./img/items/128.webp"
     },
     {
         "bgColor": "radial-gradient(circle, rgba(255,95,95,1) 0%, rgba(25,2,92,1) 100%)",
         "name": "Turtle",
-        "imageURL": "./img/items/20.png"
+        "imageURL": "./img/items/130.webp"
     }
 ]
